496e8bc9652f0c8cf081e66702afb51c35002912
Gary Dhillon <me@garydhillon.com>
2019-08-21 17:32:02 +0000

    commit 3

--- src/main/java/customer/CloudantPropertiesBean.java
+++ src/main/java/customer/CloudantPropertiesBean.java
@@ -1,6 +1,5 @@
 package customer;
-import org.springframework.boot.context.properties.Configu
-rationProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(prefix="spring.cloudant")
